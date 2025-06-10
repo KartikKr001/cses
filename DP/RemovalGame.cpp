f89f8bc8091151ba941a2030c21a09a1f778a972
Kartik Kumar <kartikkrprajapati01@gmail.com>
2025-06-10 19:11:36 +0530

    Removal Game

--- DP/RemovalGame.cpp
+++ DP/RemovalGame.cpp
@@ -1,20 +1,34 @@
-#include<bits/stdc++.h>
+#define pp pair<ll,ll>
+#define ll long long int
+#include <bits/stdc++.h>
+#define MOD 1000000007
 using namespace std;
-long long dp[5003][5003];
-vector<int>num;
-long long f(int i,int j){
-    if(i>j) return 0;
-    if(dp[i][j] != -1) return dp[i][j];
-    return dp[i][j] = max({min(num[i]+f(i+2,j),num[i]+f(i+1,j-1)),min(num[j]+f(i+1,j-1),num[j]+f(i,j-2))});
+
+ll dp[5003][5003][2];
+vector<ll>v;
+long long f(int i,int j,bool flag){
+    if(i > j) return 0;
+    if(dp[i][j][flag] != 1e10) return dp[i][j][flag];
+    if(flag){
+        return dp[i][j][flag] = max(v[i]+f(i+1,j,!flag),v[j]+f(i,j-1,!flag));
+    }
+    else{
+        return dp[i][j][flag] = min(f(i+1,j,!flag),f(i,j-1,!flag));
+    }
 }
 int main(){
     int n;
     cin>> n;
-    num.clear();
-    num.resize(n);
-    for(int i=0;i<n;i++) cin>>num[i];
-    memset(dp,-1,sizeof dp);
-    cout<<f(0,n-1)<<endl;
+    v.clear();
+    v.resize(n);
+    for(int i=0;i<n;i++) cin>>v[i];
+    for(int i=0;i<5003;i++){
+        for(int j=0;j<5003;j++){
+            dp[i][j][0] = 1e10;
+            dp[i][j][1] = 1e10;
+        }
+    }
+    cout<<f(0,n-1,1)<<endl;
     return 0;
 
 }
